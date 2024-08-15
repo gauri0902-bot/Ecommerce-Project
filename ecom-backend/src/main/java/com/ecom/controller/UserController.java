d6e03250cc2d3c11ce6631b60d064ff4f1f8cf34
Your Name <you@example.com>
2024-08-15 18:41:54 +0530

    final

---
+++ ecom-backend/src/main/java/com/ecom/controller/UserController.java
@@ -0,0 +1,49 @@
+package com.ecom.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecom.entity.User;
+import com.ecom.service.UserService;
+
+import javax.annotation.PostConstruct;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostConstruct
+    public void initRoleAndUser() {
+        userService.initRoleAndUser();
+    }
+
+    @PostMapping({"/registerNewUser"})
+    public User registerNewUser(@RequestBody User user) {
+        return userService.registerNewUser(user);
+    }
+    @GetMapping("/get")
+    public String fun() {
+    	return "hello";
+    }
+
+    @GetMapping({"/forAdmin"})
+    @PreAuthorize("hasRole('Admin')")
+    public String forAdmin(){
+        return "This URL is only accessible to the admin";
+    }
+
+    @GetMapping({"/forUser"})
+    @PreAuthorize("hasRole('User')")
+    public String forUser(){
+        return "This URL is only accessible to the user";
+    }
+}
---
+++ ecom-backend/src/test/java/com/rak/jwt/JwtYoutubeApplicationTests.java
@@ -0,0 +1,15 @@
+package com.rak.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.ecom.Ecomm;
+
+@SpringBootTest(classes = Ecomm.class)
+class JwtYoutubeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
